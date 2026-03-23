88b39cfb534fb637ca4d0d91380726019b54a33e
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-03-23 22:07:31 +0530

    flutter widgets rectangle decoration

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+
+              color: Colors.grey[300],
+              shape: BoxShape.rectangle,
+             boxShadow: [
+               BoxShadow(
+                blurRadius: 15,
+                color: Colors.grey.shade500,
+                offset: Offset(4,4),
+                spreadRadius: 1.0
+              ),
+              BoxShadow(
+                blurRadius: 15,
+                color: Colors.white,
+                offset: Offset(-4,-4),
+                spreadRadius: 1.0
+              ),
+               BoxShadow(
+                blurRadius: 15,
+                color: Colors.white,
+                offset: Offset(-4,-4),
+                spreadRadius: 1.0
+              )
+             ]
+            ),
+            
+          
+          ),
+        ),
+      ),
+    );
+  }
+}
