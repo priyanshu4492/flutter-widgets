3b5d3e3561c8899d453003f0da2ba6fa4b61ddc3
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-03-24 23:20:41 +0530

    bottom navigation bar item

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  void _navigatebottombar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    //you can also add different pages like homepage(),firstpage(), by creating different pages and calling it here//
+    Center(child: Text("home", style: TextStyle(fontSize: 50))),
+    Center(child: Text("settings", style: TextStyle(fontSize: 50))),
+    Center(child: Text("message", style: TextStyle(fontSize: 50))),
+    Center(child: Text("account", style: TextStyle(fontSize: 50))),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.grey[200],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          onTap: _navigatebottombar,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: 'message',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+          ],
+        ),
+      ),
+    );
+  }
+}
