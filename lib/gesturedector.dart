3b744db35b48f2adb489d79bb5b6317e00050492
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-03-24 22:19:15 +0530

    gesture dectector

---
+++ lib/gesturedector.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int numberOfTimesTapped = 0;
+
+  void increasenumber() {
+    setState(() {
+      numberOfTimesTapped++;
+    });
+  }
+
+  void decreaseNumber() {
+    setState(() {
+      numberOfTimesTapped--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 173, 125, 231),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Tapped" + numberOfTimesTapped.toString() + "times ",
+                style: TextStyle(fontSize: 20),
+              ),
+              GestureDetector(
+                onTap: increasenumber,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.red,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 12,
+                        color: Colors.red,
+                        spreadRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    "click here",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ),
+              ),
+
+              GestureDetector(
+                onTap: decreaseNumber,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 12,
+                        color: Colors.green,
+                        spreadRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Text("click to minus", style: TextStyle(fontSize: 40)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/gridview_builder.dart
@@ -0,0 +1,30 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext ontext) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: GridView.builder(
+// physics: NeverScrollableScrollPhysics(),
+//           itemCount: 20,
+//           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//             crossAxisCount: 3,
+//           ),
+//           itemBuilder: (context, index) {
+//             return Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: Container(height: 200, width: 200, color: Colors.red),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
