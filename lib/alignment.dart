119be89203a1ef63f6fc72d891d09c2529e460d2
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-03-24 21:27:58 +0530

    clipprreact

---
+++ lib/alignment.dart
@@ -0,0 +1,34 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(AlignmentApp());
+// }
+
+// class AlignmentApp extends StatelessWidget {
+//   const AlignmentApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Row(
+//            crossAxisAlignment: CrossAxisAlignment.end,
+//               mainAxisAlignment: MainAxisAlignment.end,
+//           children: [
+            
+             
+                
+//                 Container(height: 200, width: double.infinity, color: Colors.red),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Container(height: 100, width: 200, color: Colors.red),
+//                 ),
+//                 Container(height: 50, width: 100, color: Colors.red),
+//               ],
+            
+          
+//         ),
+//       ),
+//     );
+//   }
+// }
