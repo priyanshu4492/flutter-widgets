d1938d4d8e272daf53ff47a7e84bd15c3ee19421
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-03-26 22:38:05 +0530

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,33 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.pink[100],
+                alignment: Alignment(0, 0),
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.pink,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
